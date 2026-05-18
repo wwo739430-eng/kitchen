c6f56da4d89fe6d488b74dd1987e1a591b18a281
wwo739430-eng <wwo739430@gmail.com>
2026-05-18 17:00:29 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,24 @@
-/**
- * Main App Component
- * 应用主入口，配置路由
- */
-import ScanReceipt from './pages/ScanReceipt';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Home from './pages/Home';
+import Settings from './pages/Settings';
 import MyIngredients from './pages/MyIngredients';
 import Favorites from './pages/Favorites';
 import ShoppingList from './pages/ShoppingList';
-import Settings from './pages/Settings';
 
 function App() {
   return (
     <Router>
-      <div className="flex min-h-screen bg-background-light dark:bg-background-dark text-[#141513] dark:text-white doodle-bg">
+      <div className="flex min-h-screen bg-[#fdfdfb] doodle-bg">
+        {/* 左侧导航 */}
         <Sidebar />
+
+        {/* 主内容区 */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/ingredients" element={<MyIngredients />} />
           <Route path="/favorites" element={<Favorites />} />
           <Route path="/shopping-list" element={<ShoppingList />} />
-          <Route path="/scan" element={<ScanReceipt />} />
           <Route path="/settings" element={<Settings />} />
         </Routes>
       </div>
